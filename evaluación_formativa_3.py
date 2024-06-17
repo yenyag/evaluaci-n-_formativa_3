50ca06988c8289b9b84abb30a03ab743124a2de2
TheRexInformatics <166179751+987123546@users.noreply.github.com>
2024-06-17 12:09:00 -0400

    Update evaluación_formativa_3.py

---
+++ evaluación_formativa_3.py
@@ -0,0 +1,39 @@
+
+listaTrabajadores=[]
+listaCargos=[]
+listaSueldoBruto=[]
+
+#Aquí va el Menú
+while True:
+    print("\n---Menú de Opciones---\n")
+    print("Opción 1. Registrar trabajador")
+    print("Opción 2. Listar a todos los trabajadores")
+    print("Opción 3. Imprimir planilla de sueltos");
+    print("Opción 4. Salir del Programa");
+    try:
+        opcion=int(input("Ingrese la Opción que deseas utilizar ----> "));
+    except ValueError:
+        print("Ingrese una opción válida, desde 1 a 4")
+    else:
+        if opcion==1:
+            print("Bienvenido empleado, por favor registrese: ");
+            
+
+        elif opcion==2:
+            print();
+
+
+        elif opcion==3:
+            print();
+
+
+        elif opcion==4:
+            print();
+            break;
+
+        else:
+                print("Debes ingresar opciones desde 1 a 4");
+
+
+
+
