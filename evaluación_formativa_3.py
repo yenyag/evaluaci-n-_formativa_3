b404719258c65cf2f8c32f35ba4f30bfe5793b15
benjamin8ue <benjaminalmo943@gmail.com>
2024-06-17 18:22:59 -0400

    Update evaluación_formativa_3.py

--- evaluación_formativa_3.py
+++ evaluación_formativa_3.py
@@ -1,24 +1,53 @@
 #importaciones
 import time;
-import csv
 
-def Registrar_Trabajador(trabajadores):
-    Nombre = input("Ingrese Nombre")
-    Apellido = input("Ingrese Su Apellido")
-    cargo = input("ingrese su cargo")
-    sueldo_bruto = int(input("ingrese su sueldo bruto"))
+class Trabajador:
+    def __init__(self, nombre, apellido, cargo, sueldo_bruto):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.cargo=cargo
+        self.sueldo_bruto=sueldo_bruto
+        self.desc_salud=calcular_salud
+        self.desc_afp=calcular_afp
+        self.liquido_a_pagar=calcular_liquido
+        
+    def calcular_salud(self):
+        return self.sueldo_bruto*0.07
 
-def Trabajador = Trabajador(Nombre, Apellido, Cargo, Sueldo_Bruto)
-    trabajadores.append(trabajadores)
+
+    def calcular_afp(self):
+        return self.sueldo_bruto*0.12
+
+
+    def calcular_liquido(self):
+        return self.calcular_liquido
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}, {self.cargo}, Sueldo Bruto: {self.sueldo_bruto}, Desc. Salud: {self.desc_salud}, Desc. AFP: {self.desc_afp}, Liquidio a Pagar: {self.liquido_a_pagar}"
+
+def Registrar_Trabajador(Trabajadores):
+    nombre = input("Ingrese Nombre: ")
+    apellido = input("Ingrese Apellido")
+    cargo   = input ("Ingrese cargo: ") 
+    sueldo_bruto = int(input ("Ingrese sueldo bruto"))
+
+    trabajador = trabajador(nombre, apellido, cargo, sueldo_bruto)
+    Trabajadores.append(trabajador)
+
+def listar_trabajadores(Trabajadores):
+    for trabajador in Trabajadores:
+        print(trabajador)
+
 def imprimir_plantilla(Trabajadores):
     cargo=input("Ingrese el cargo para imprimir la plantilla o 'todos' para imprimir todos: ")
-    pancracio = "plantilla.txt"
-     with open(nombre_archivo, "w") as file:
+    Pancracio = "plantilla.txt"
+        
+    with open(Pancracio, "w") as file:
         file.write("Trabajador, Cargo, Sueldo Bruto, Desc. Salud, Desc. AFP, Liquido a pagar\n")
         for trabajador in Trabajadores:
             if cargo.lower() == 'todos' or trabajador.cargo.lower() == cargo.lower():
                 file.write(f"{trabajador.nombre} {trabajador.apellido}, {trabajador.cargo}, {trabajador.sueldo_bruto},
-                           {trabajador.desc_salud}, {trabajador.desc_afp}, {trabajador.liquido_a_pagar}\n")
+                             {trabajador.desc_salud}, {trabajador.desc_afp}, {trabajador.liquido_a_pagar}\n")
     print(f"Plantilla generada en {Pancracio}")
 
 
